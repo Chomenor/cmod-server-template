8d34b1066b64512f26530a1c1ba22670af7d0a65
Noah Metzger <chomenor@gmail.com>
2024-10-21 17:15:27 -0500

    change resource loader indent level from 4 to 2 spaces

--- resource_loader/run_export.py
+++ resource_loader/run_export.py
@@ -9,17 +9,17 @@ import sys
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 def process():
-    output_path = script_directory + "/output"
+  output_path = script_directory + "/output"
 
-    # Load manifest
-    manifest = export.Manifest()
-    manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/base.json"))
-    manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/efmaps.json"))
-    manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/mod_resources.json"))
+  # Load manifest
+  manifest = export.Manifest()
+  manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/base.json"))
+  manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/efmaps.json"))
+  manifest.import_manifest(misc.read_json_file(f"{script_directory}/common/export/profiles/mod_resources.json"))
 
-    # Additional directories to look for resources with hash as filename
-    local_dirs = []
+  # Additional directories to look for resources with hash as filename
+  local_dirs = []
 
-    export.run_export(manifest, output_path, local_dirs)
+  export.run_export(manifest, output_path, local_dirs)
 
 process()
